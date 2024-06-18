2309e5030f023c0d2524bf6725b7ca96e17281cb
alina-gasperino <alinacreatesthings@gmail.com>
2024-06-18 04:48:18 -0400

    custom PDP

--- assets/custom.js
+++ assets/custom.js
@@ -8,16 +8,28 @@ jQuery(document).ready(function ($) {
         })
     })
 
-    $(".option_field.ring-sizes").append($(".keyideas.price"))
+    $(".option_field.ring-sizes").append($(".keyideas.price"));
+    $(".ring_quantity").append($(".quantity-wrapper"));
 
     setTimeout(() => {
         if($(".keyideas.price").length > 0) {
-            $(".line-item-property__field").show()
+            $(".line-item-property__field").show();
+            $(".options--shape").closest(".option--color-metal-shape").hide();
         }
     }, 500);
 
     $("#carat-size").change(function () { 
         console.log($(this).val())
     });
+
+    $(".radio_boxes label").each(function() {
+        $(this).click(function(){
+            $(this).closest(".line-item-property__field").find("label").find("span").text($(this).text());
+            $(".radio_boxes label").each(function() {
+                $(this).removeClass("selected")
+            })
+            $(this).addClass("selected");
+        })
+    })
 
 });
