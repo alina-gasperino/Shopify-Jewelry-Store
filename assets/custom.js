791f4e28493e8e44fcdae1e66ea8920fc34dbabb
alina-gasperino <alinacreatesthings@gmail.com>
2024-12-10 02:33:50 -0500

    new version of the theme

--- assets/custom.js
+++ assets/custom.js
@@ -8,13 +8,14 @@ jQuery(document).ready(function ($) {
         })
     })
 
-    $(".option_field.ring-sizes").append($(".keyideas.price"));
-    $(".ring_quantity").append($(".quantity-wrapper"));
-
-    setTimeout(() => {
-        if($(".keyideas.price").length > 0) {
+    var timer = setInterval(() => {
+        if($(".keyideas.reviewprice").length > 0) {
+            clearInterval(timer)
             $(".line-item-property__field").show();
             $(".options--shape").closest(".option--color-metal-shape").hide();
+            $(".option_field.ring-sizes").append($(".keyideas.reviewprice"));
+            $(".ring_quantity").append($(".quantity-wrapper"));
+            $(".option-selectors").hide()
         }
     }, 500);
 
@@ -38,4 +39,24 @@ jQuery(document).ready(function ($) {
     $(".calendar").click(function() {
         $(".bookeasy-button-container button").click();
     })
+
+    $('.template-suffix-studs .options--carat-total-weight').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        infinite: false
+    });
+    function updateClickableButtons() {
+        $(".prev_var").toggleClass("slick-disabled", $(".options--carat-total-weight .slick-prev").attr("aria-disabled") === "true");
+        $(".next_var").toggleClass("slick-disabled", $(".options--carat-total-weight .slick-next").attr("aria-disabled") === "true");
+    }
+    
+    updateClickableButtons();
+    
+    $(".prev_var, .next_var").click(function() {
+        const isPrev = $(this).hasClass("prev_var");
+        $(".options--carat-total-weight ." + (isPrev ? "slick-prev" : "slick-next")).click();
+        updateClickableButtons();
+    });
 });
